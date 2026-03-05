26ae73555391f601ff28ce379fcfc77a4b88c6b1
caio <cp.albuquerque018@gmail.com>
2026-03-05 10:34:00 -0500

    Initial Commit

---
+++ RouteCast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RouteCast
+//
+//  Created by Caio Albuquerque on 3/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RouteCast/RouteCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteCastApp.swift
+//  RouteCast
+//
+//  Created by Caio Albuquerque on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct RouteCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
