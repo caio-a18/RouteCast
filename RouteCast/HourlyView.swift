21cd8a2103f56df4dc510bb8dd6e0daa2ac0af53
kagersophia <159161703+kagersophia@users.noreply.github.com>
2026-04-10 12:25:44 -0500

    Edits to new files

--- RouteCast/HourlyView.swift
+++ RouteCast/HourlyView.swift
@@ -4,5 +4,10 @@
 //
 //  Created by Sophia Kager on 4/10/26.
 //
-
+import SwiftUI
 import Foundation
+struct HourlyView: View {
+    var body: some View {
+        Text("Hourly Screen")
+    }
+}
--- RouteCast/RouteView.swift
+++ RouteCast/RouteView.swift
@@ -4,5 +4,11 @@
 //
 //  Created by Sophia Kager on 4/10/26.
 //
-
+import SwiftUI
 import Foundation
+
+struct RouteView: View {
+    var body: some View {
+        Text("Route Screen")
+    }
+}
